32ba46f870c2bbca1fb22d6acedcd3b2258f1ef9
Seven 7️⃣ <57207085+sevenppn@users.noreply.github.com>
2026-06-13 09:54:49 +0700

    Create basic service worker for PWA
    Initial service worker setup for PWA support.

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker เบื้องต้นเพื่อให้เบราว์เซอร์ยอมรับว่าเป็น PWA App
+self.addEventListener('fetch', function(event) {
+    // สามารถปล่อยว่างไว้ หรือใส่เพื่อทำระบบ Cache ในอนาคตได้
+});
